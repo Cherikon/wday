3f3df4177ac27f959f6262566eac3aa0ce8892e4
Sergey Ikonnikov <ikonnikov.Sergeyy@yandex.ru>
2026-03-06 00:31:11 +0500

    поправил путь для скачивания гифок

--- src/utils/preDownloadGifs.js
+++ src/utils/preDownloadGifs.js
@@ -2,7 +2,7 @@ import {gifsUrl} from "../components/gifs";
 
 export const preDownloadGifs = (setState) => {
     gifsUrl.forEach(async (gifUrl) => {
-        const response = await fetch(gifUrl);
+        const response = await fetch(location.href.includes('wday') ? `/wday${gifUrl}` : gifUrl);
         const blob = await response.blob();
         const reader = new FileReader();
 
